4bb84799d3bedf1d659b922a4fe441fbda17264d
bijan ebrahimy <myopensourceprojects@gmail.com>
2014-05-19 19:30:54 +0430

    migrated to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519082247) do
+ActiveRecord::Schema.define(version: 20140519144547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.integer  "user_id",                      null: false
+    t.string   "slug",                         null: false
+    t.string   "name",                         null: false
+    t.datetime "due_date",                     null: false
+    t.boolean  "confirmation", default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
