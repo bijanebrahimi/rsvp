5f57e5ed3515be2d32e7c667e5c7a0899117cdcd
bijan ebrahimy <myopensourceprojects@gmail.com>
2014-05-20 12:52:22 +0430

    added events#update

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,7 @@ class Event < ActiveRecord::Base
   validates :name, :presence =>true
   validates :description, :presence =>true
   validates :due_date, :presence =>true
+  
+  # Default Order
+  default_scope order('created_at DESC')
 end
